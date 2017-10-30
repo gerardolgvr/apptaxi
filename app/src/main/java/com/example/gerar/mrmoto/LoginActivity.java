2403244c7cd346bc0e76679e164d758135326dad
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2017-10-30 02:49:47 -0500

    Vista login terminada

--- app/src/main/java/com/example/gerar/mrmoto/LoginActivity.java
+++ app/src/main/java/com/example/gerar/mrmoto/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.gerar.mrmoto;
 
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void goCreateAccount(View view){
+
+    }
 }
+
