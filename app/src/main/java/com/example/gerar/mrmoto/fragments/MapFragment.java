e8fd7f0cf6d5795d97c0216d6653e34cf8fefffa
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2017-11-26 18:51:51 -0500

    Preparacion de mapas lista y ui

---
+++ app/src/main/java/com/example/gerar/mrmoto/fragments/MapFragment.java
@@ -0,0 +1,57 @@
+package com.example.gerar.mrmoto.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gerar.mrmoto.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+    private View rootView;
+    private GoogleMap gMap;
+    private MapView mapView;
+
+
+    public MapFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView =  inflater.inflate(R.layout.fragment_map, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) rootView.findViewById(R.id.map);
+        if(mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
--- app/src/main/java/com/example/gerar/mrmoto/view/HomeActivity.java
+++ app/src/main/java/com/example/gerar/mrmoto/view/HomeActivity.java
@@ -1,13 +1,68 @@
 package com.example.gerar.mrmoto.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.gerar.mrmoto.R;
+import com.example.gerar.mrmoto.fragments.HistoryFragment;
+import com.example.gerar.mrmoto.fragments.MapFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Fragment currentFragment;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    currentFragment = new MapFragment();
+                    changeFragment(currentFragment);
+                    return true;
+                case R.id.navigation_history:
+                    currentFragment = new HistoryFragment();
+                    changeFragment(currentFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void changeFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        showToolbar("Mr Moto", false);
+
+        if(savedInstanceState == null){
+            currentFragment = new MapFragment();
+            changeFragment(currentFragment);
+        }
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    public void showToolbar(String title, boolean upButton){
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+
+
+
 }
